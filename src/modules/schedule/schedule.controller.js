83b5f95ea74d84fe31c4d1d0d264e0bccb5ca778
ghufron akbar <alan.040219.gam@gmail.com>
2025-05-09 03:24:50 +0700

    Add New Webhook Logic

--- src/modules/schedule/schedule.controller.js
+++ src/modules/schedule/schedule.controller.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cron from "node-cron"
 import { sendWhatsapp } from "../../lib/whatsapp.js";
-import { getSchedule } from "./schedule.service.js"
+import { getSchedule, switchMode } from "./schedule.service.js"
 import { convertHour } from "../../helper/convert-hour.js";
 
 const router = express.Router();
@@ -44,8 +44,30 @@ export const scheduleTask = async () => {
 
 router.post("/webhook", async (req, res) => {
     try {
-        console.log("Received schedule request");
+        console.log("Received webhook request");
         console.log(req.body);
+        const { message, sender } = req.body
+        if (sender === "6285156031385" || sender === "085156031385") {
+            if (message) {
+                if (message.includes("REFRESH")) {
+                    await scheduleTask();
+                    await sendWhatsapp("Refreshed schedule");
+                } else if (message.includes("MODE:")) {
+                    const mode = message.split(":")[1].trim();
+                    try {
+                        await switchMode(mode);
+                        await scheduleTask();
+                        await sendWhatsapp(`Refreshed schedule to ${mode}`);
+                    } catch (error) {
+                        console.error("Error while setting mode:", error);
+                        await sendWhatsapp(`Error while setting mode: ${error?.message || error}`);
+                    }
+                } else {
+                    console.log("INVALID COMMAND");
+                    // await sendWhatsapp("Invalid command");
+                }
+            }
+        }
         return res.status(200).json({
             status: 200, message: "Success", req: {
                 body: req.body,
@@ -62,7 +84,7 @@ router.post("/webhook", async (req, res) => {
 
 router.get("/webhook", async (req, res) => {
     try {
-        console.log("Received schedule request");
+        console.log("Received webhook request");
         console.log(req.body);
         return res.status(200).json({
             status: 200, message: "Success", req: {
