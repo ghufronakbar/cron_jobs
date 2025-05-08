db30e1bc4126606f50495ab2feca1e3824f2aa14
ghufron akbar <alan.040219.gam@gmail.com>
2025-05-09 02:59:15 +0700

    debugging

--- src/helper/convert-hour.js
+++ src/helper/convert-hour.js
@@ -1,5 +1,7 @@
 export const convertHour = (hour) => {
     const numHour = Number(hour);
     const utcHour = (numHour - 7 + 24) % 24;
-    return utcHour.toString().padStart(2, '0');
+    const finalHour = utcHour.toString().padStart(2, '0');
+    console.log("BEFORE: ", hour, "AFTER: ", finalHour);
+    return finalHour;
 };
